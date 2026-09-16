6ede8fd7000dfd8a5638e38bd97b7f16bf865870
Christomanos Anastasiou <hristoma@gmail.com>
2026-09-16 15:58:01 +0100

    Add Dynamix Tactical Formations and DirectX11 Projects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Bowling Fury: Ten Pin King",
     year: "2023 – 2024 · Released",
     description:
-      "Competitive and fast paced mobile bowling game with over 500K installs and 4.6 average rating on app stores.",
+      "Competitive and fast-paced mobile bowling game with over 500K installs and 4.6 average rating on app stores.",
     longDescription:
       "As a Junior Software Developer, I contributed to the development of Bowling Fury by tackling a variety of tasks across the codebase. I implemented new features, optimized performance, and fixed bugs to enhance the overall gaming experience. I also collaborated closely with the product and art teams to ensure seamless integration of assets and smooth gameplay. With 1M+ installs across multiple platforms, I am proud to have been part of a team that was diligent, passionate and professional, and I gained valuable experience in programming, software architecture and teamwork.",
     tech: ["C#", "Unity","GCP", "Redis", "Kubernetes", "Firebase", "TeamCity", "DOTween"],
@@ -54,28 +54,47 @@ export const projects = [
     featured: false,
     category: "unity",
   },
-  // {
-  //   id: "project-four",
-  //   title: "Project Delta",
-  //   description:
-  //     "API service with caching, observability, and deployment on a cloud provider.",
-  //   longDescription:
-  //     "Cover architecture, scaling considerations, and monitoring setup.",
-  //   tech: ["Go", "Redis", "AWS"],
-  //   image: "/images/project-4.svg",
-  //   links: {
-  //     live: null,
-  //     github: "https://github.com/yourusername/project-delta",
-  //   },
-  //   featured: false,
-  //   category: "backend",
-  // },
+  {
+    id: "project-four",
+    title: "Dynamic Tactical Formations in Video Games",
+    description:
+      "Postgraduate research project exploring dynamic tactical formations for units in real-time strategy games, focusing on obstacle avoidance and pathfinding algorithms.",
+    longDescription:
+      "Curious about how AI agents behave in groups and a fan of real-time strategy games, I embarked on a research project to explore dynamic tactical formations. " + 
+      "Using Unity and C#, I developed a 3D obstacle course that allows players to control units in various formations while navigating complex environments with obstacle avoidance and pathfinding algorithms to ensure coherent movement of units while maintaining their formations." + 
+      "Implementing dynamic tactical formations in real-time strategy games presented several challenges. The primary challenge is ensuring that units maintain their formation while navigating complex environments with obstacles and changing terrain. " + 
+      "Additionally, the system must be responsive to player commands and adapt to evolving battlefield conditions.",
+    tech: ["Unity3D", "C#", "Unit management", "Pathfinding", "Obstacle avoidance"],
+    image: "/images/head_to_head.png",
+    links: {
+      live: null,
+      github: "https://github.com/hristomanos/DynamicTacticalFormations",
+    },
+    featured: false,
+    category: "unity",
+  },
+  {
+    id: "project-five",
+    title: "DirectX 11 Graphics Techniques Showcase",
+    description:
+      "A DirectX 11 application demonstrating various advanced graphics techniques.",
+    longDescription:
+      "DirectX 11 is a low-level C++ rendering API that enables us to create visually stunning real-time applications. Interacting with DirectX 11 requires a deep understanding of graphics programming concepts. Implementing those concepts as part of my university course was the best way for me learn. This project features normal mapping, parallax mapping, post-processing effects and lighting. Additionally, procedural terrain generation was implemented using a heighmap. However, it resulted to sharp edges and a lack of natural-looking terrain.The solution was to implement the Diamond-Square algorithm for terrain smoothing, which resulted in a more natural-looking landscape with smoother edges and better overall terrain quality." , 
+    tech: ["DirectX 11", "C++", "IMGUI", "Graphics programming", "Shader development", "Lighting", "Post-processing", "Normal mapping", "Parallax mapping", "Procedural terrain generation"],
+    image: "/images/parallax.png",
+    links: {
+      live: null,
+      github: "https://github.com/hristomanos/DirectX11",
+    },
+    featured: false,
+    category: "directX11",
+  },
 ];
 
 export const projectCategories = [
   { id: "all", label: "All" },
   { id: "shippedTitles", label: "Shipped Titles" },
   { id: "unity", label: "Unity" },
-  //{ id: "backend", label: "Backend" },
+  { id: "directX11", label: "DirectX 11" },
   //{ id: "tools", label: "Tools" },
 ];
